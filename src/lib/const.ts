4a7199a7ff991dcd0665d8e5b4f9238b9003263a
ohohaustin1 <ohohaustin1@gmail.com>
2026-04-08 19:09:52 +0800

    Add src/lib/const.ts

---
+++ src/lib/const.ts
@@ -0,0 +1,5 @@
+// Constants
+export const API_BASE_URL = process.env.VITE_API_URL || 'http://localhost:3001';
+export const APP_NAME = 'ORION';
+export const appName = APP_NAME;
+export const getLoginUrl = () => '/login';
