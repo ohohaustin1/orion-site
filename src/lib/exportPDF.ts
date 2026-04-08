21ed9159a508a78d6af340c7c8bb9ebbecd67731
ohohaustin1 <ohohaustin1@gmail.com>
2026-04-08 19:09:57 +0800

    Add src/lib/exportPDF.ts

---
+++ src/lib/exportPDF.ts
@@ -0,0 +1,8 @@
+// PDF export stub
+export const exportPDF = async (element: HTMLElement, filename: string) => {
+  console.log('Export PDF:', filename);
+};
+
+export const exportAnalysisPDF = async (data: any) => {
+  console.log('Export analysis PDF:', data);
+};
